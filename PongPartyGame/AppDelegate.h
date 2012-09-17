ae84229536824f01934234275fb18132f4870af4
Matthew Baranowski <mfbaranow@gmail.com>
2012-09-17 10:43:43 -0400

    Initial Commit

---
+++ PongPartyGame/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PongPartyGame
+//
+//  Created by Matthew Baranowski on 9/17/12.
+//  Copyright (c) 2012 Matthew Baranowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
